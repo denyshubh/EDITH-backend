a06b24a06e5784137212c626bde8c2d1ee9f75b7
Tonmoy Kalita <54646996+Tonmoy-K@users.noreply.github.com>
2020-06-01 12:59:40 +0530

    update car_table

--- migrations/20200601061055-car.js
+++ migrations/20200601061055-car.js
@@ -9,6 +9,33 @@ module.exports = {
       Example:
       return queryInterface.createTable('users', { id: Sequelize.INTEGER });
     */
+    return queryInterface.sequelize.query('CREATE EXTENSION if not exists pgcrypto').then(() => {
+      return queryInterface.createTable('car', {
+        vin: {
+          type: Sequelize.STRING,
+          allownull: false,
+          primaryKey: true
+        },
+        make_id: {
+          type: Sequelise.STRING,
+          references: "make"
+          referencesKey: "make_id"
+        },
+         model_id: {
+          type: Sequelise.STRING,
+          references: "model"
+          referencesKey: "model_id"
+        },
+        year: Sequelise.INTEGER(4),
+        ecrgradeid: Sequelise.INTEGER(3),
+        mileage: Sequelise.INTEGER(8),
+        saleurl: Sequelise.STRING,
+        location_id: {
+          type: Sequelise.STRING,
+          references: "location"
+          referencesKey: "location_id"
+        },
+        createdate: Sequelise.DATE
   },
 
   down: (queryInterface, Sequelize) => {
@@ -19,5 +46,6 @@ module.exports = {
       Example:
       return queryInterface.dropTable('users');
     */
+        return queryInterface.dropTable('car')
   }
 };
