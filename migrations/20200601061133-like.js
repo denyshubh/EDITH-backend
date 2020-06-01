397f143e018c333a9d4e1a10212489b841b342a2
Tonmoy Kalita <54646996+Tonmoy-K@users.noreply.github.com>
2020-06-01 13:31:43 +0530

    Update Migration liked

--- migrations/20200601061133-like.js
+++ migrations/20200601061133-like.js
@@ -2,13 +2,26 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+   return queryInterface.sequelize.query('CREATE EXTENSION if not exists pgcrypto').then(() => {
+      return queryInterface.createTable('liked', {
+        like_id: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          primaryKey: true
+        },
+        user_id: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          references: 'user',
+          referencesKey: 'user_id'
+        },
+        vin: {
+          type: Sequelize.STRING,
+          allowNull: false
+        },
+        liked: Sequelize.INT(1)
+      })
+   })
   },
 
   down: (queryInterface, Sequelize) => {
@@ -19,5 +32,6 @@ module.exports = {
       Example:
       return queryInterface.dropTable('users');
     */
+    return queryInterface.dropTable('liked')
   }
 };
